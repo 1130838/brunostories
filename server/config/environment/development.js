d6ee4bc8d5010fb5e3615eb842ed84e44d54f067
bruno devesa <brunodevesa@mbp-de-bruno.lan>
2015-09-03 23:03:10 +0100

    change for remote database mongolab server

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,8 +5,12 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
+    // remote mongolab database url:
     uri: 'mongodb://heroku_4bfll4jp:216r8hqtl6bkdln2dn0t0vm4lt@ds035543.mongolab.com:35543/heroku_4bfll4jp'
   },
 
   seedDB: true
 };
+
+//change for this url to use on local db - must run this first! :  ./mongod --dbpath /Users/brunodevesa/IdeaProjects/WebProjects/brunostories/data/db
+//'mongodb://localhost/brunostories-dev'
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -18,6 +18,11 @@ module.exports = {
     uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            // remote mongolab database url:
             'mongodb://heroku_4bfll4jp:216r8hqtl6bkdln2dn0t0vm4lt@ds035543.mongolab.com:35543/heroku_4bfll4jp'
   }
 };
+
+
+//change for this url to use on local db - must run this first! :  ./mongod --dbpath /Users/brunodevesa/IdeaProjects/WebProjects/brunostories/data/db
+//'mongodb://localhost/brunostories-dev'
