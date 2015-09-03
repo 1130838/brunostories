1b5573b041527aa1ead61bf758357761419e7bb5
bruno devesa <brunodevesa@mbp-de-bruno.lan>
2015-09-03 20:45:26 +0100

    Built brunostories from commit 5b728dc on branch master

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://heroku_4bfll4jp:216r8hqtl6bkdln2dn0t0vm4lt@ds035543.mongolab.com:35543/heroku_4bfll4jp'
+  },
+
+  seedDB: true
+};
---
+++ server/config/environment/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Test specific configuration
+// ===========================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://heroku_4bfll4jp:216r8hqtl6bkdln2dn0t0vm4lt@ds035543.mongolab.com:35543/heroku_4bfll4jp'
+  }
+};
