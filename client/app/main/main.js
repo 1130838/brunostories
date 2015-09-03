5b728dc0f3ab5d7ba317c87fd59f14f4d98b672e
bruno devesa <brunodevesa@mbp-de-bruno.lan>
2015-09-03 18:53:22 +0100

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('brunostoriesApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
