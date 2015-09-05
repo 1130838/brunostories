9523d5a0ebff4b763dc06ec0d7b0a50998302ac1
bruno devesa <brunodevesa@mbp-de-bruno.lan>
2015-09-05 19:13:14 +0100

    Built brunostories from commit 666f4a4 on branch master

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
 
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
@@ -19,6 +19,8 @@ module.exports = function(app) {
   // All other routes should redirect to the index.html
   app.route('/*')
     .get(function(req, res) {
-      res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+      //res.sendFile(path.resolve(app.get('appPath') + '/index.html'));
+      res.sendfile(app.get('appPath') + '/index.html');
+
     });
 };
